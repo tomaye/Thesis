a55c8646f439b4f87d35fb3439eda15310807739
frigori <soeinheld71@gmx.de>
2016-09-05 18:18:58 +0200

    05.09

--- plotWordFreq.py
+++ plotWordFreq.py
@@ -1,32 +1,78 @@
-from nltk import FreqDist # used later to plot and get count
-from nltk.tokenize import word_tokenize # tokenizes our sentence by word
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import defaultdict
 from tkinter.filedialog import askopenfilename
 import os
 
 
-#fdist = FreqDist(word.lower() for word in word_tokenize(text))
-
-#tknz = word_tokenize(text)
-#fdist = FreqDist(tknz)
 
 
 
 
 
-
-datapath = askopenfilename(initialdir= os.getcwd(), title="Select ASR file")
-file = open(datapath)
-lines = [line.strip().split('\t') for line in file]
-
-text = ""
-
-for line in lines:
-    print(line)
-    text += line[1]
-    text += line[2]
+#datapath = askopenfilename(initialdir= os.getcwd(), title="Select ASR file")
 
 
-print(text)
 
-#fdist = FreqDist(word.lower() for word in word_tokenize(text))
-#fdist.plot()
+def getDistribution(data, withLabel= True):
+
+    if withLabel:
+        i = 1
+    else:
+        i = 0
+
+    freq = defaultdict(int)
+
+    for line in data:
+        p1 = line[i].split()
+        p2 = line[i+1].split()
+        sentLength = len(p1) + len(p2)
+        freq[sentLength] += 1
+
+    for key in freq:
+
+       freq[key] = freq[key]/len(data)
+
+
+    return freq
+
+def checkDist(dict):
+    total = 0
+    for key in dict:
+        total += dict[key]
+
+
+
+def plotDict(d, color = "r", label ="noLabel"):
+
+    x = list(d.keys())
+    y = list(d.values())
+    plt.scatter(x, y, color=color,label=label)
+
+    #plt.plot(x, y, '-o',color=color)
+
+def main():
+
+    f1 = "data/corpus/Metalogue_Corpus_NegativePhrases.txt"
+    f2 = "data/corpus/Metalogue_extractedLinks_elaboration.txt"
+    f3 = "data/corpus/Metalogue_extractedLinks_contrast.txt"
+    f4 = "data/corpus/Metalogue_extractedLinks_fullCorpus.txt_justification.txt"
+    f5 = "data/corpus/Metalogue_extractedLinks_fullCorpus.txt_evidence.txt"
+
+    files = [f4,f5]
+    labels = ["Contrast","Elaboration","PhrasePairs"]
+    colors = list("rgb")
+
+    for file in files:
+
+        f = open(file)
+        lines = [line.strip().split('\t') for line in f]
+        d = getDistribution(lines,False)
+        plotDict(d,colors.pop(),labels.pop())
+
+    legend = plt.legend(loc='upper right', shadow=True)
+    frame = legend.get_frame()
+    frame.set_facecolor('0.90')
+    plt.show()
+
+main()
